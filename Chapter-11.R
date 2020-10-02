5cd9419876283e7b3b1ff4a1f6bda9b9228a4a14
Bijaelo <oliver.p.madsen@gmail.com>
2020-10-02 08:46:27 +0200

    Finished 11.1 - there are some things here that are known, but in a new perspective

--- Chapter-11.R
+++ Chapter-11.R
@@ -11,6 +11,7 @@ load_p <- function(x, character.only = TRUE, ...){
     Recall(x = x, character.only = character.only, ...)
   }
 }
+load_p('corrr')
 load_p('tidymodels')
 load_p('purrr')
 load_p('tidyr')
@@ -84,8 +85,53 @@ lm_ns_res <-
   add_recipe(ames_ns_rec) %>%
   fit_resamples(resamples = ames_folds, control = control_resamples(save_pred = TRUE))
 
-
+## We could do this by comparing the metrics 
+## between the fitted models
+## But as we should already know, these metrics have a tendency to be somewhat correlated
 collect_metrics(lm_ns_res)
 collect_metrics(lm_wsr)
 
+## If we collect the individual metrics from each resample
+## we can illustrate this correlation
+ns_rsq <- collect_metrics(lm_ns_res, summarize = FALSE) %>%
+  filter(.metric == 'rsq') %>%
+  select(id, 'no splines' = .estimate)
+s_rsq <- collect_metrics(lm_wsr, summarize = FALSE) %>%
+  filter(.metric == 'rsq') %>%
+  select(id, 'with splines' = .estimate)
+rf_rsq <- collect_metrics(rf_res, summarize = FALSE) %>%
+  filter(.metric == 'rsq') %>%
+  select(id, 'random forest' = .estimate)
 
+rsq_estimates <- 
+  inner_join(ns_rsq, s_rsq, by = 'id') %>%
+  inner_join(rf_rsq, by = 'id')
+
+## this shows clearly that there is quite a bit of correlation
+## We could test whether this is significant as well
+corrr::correlate(rsq_estimates %>% select(-id))
+rsq_estimates %>%
+  with(cor.test(`no splines`, `random forest`)) %>%
+  tidy() %>%
+  select(estimate, starts_with('conf'))
+## And clear this is significant
+
+## We could also visualize the effects
+## And it is quite clear that several lines
+## tend to follow the same trend across different folds
+rsq_estimates %>% 
+  pivot_longer(cols = c(-id), 
+               names_to = "model", 
+               values_to = "rsq") %>% 
+  mutate(model = reorder(model, rsq)) %>% 
+  ggplot(aes(x = model, y = rsq, group = id, col = id)) + 
+  geom_line(alpha = .5, lwd = 1.25) + 
+  labs(x = NULL, y = expression(paste(R^2, "statistics"))) + 
+  theme_pander() +
+  theme(legend.position = "none") 
+
+## Now this is something I haven't worked with in this scenario, but obviously if we were to 
+## to perform a statistical test to evaluate which model were performing better (using a test on the difference on the estimate)
+## this would cause the result to be biased as they are not independent
+
+# 11.2: Simple hypothesis test
