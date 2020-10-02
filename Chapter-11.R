340f5af5c638cd7ba881c0ad559db9e09f64cc4f
Bijaelo <oliver.p.madsen@gmail.com>
2020-10-02 10:24:32 +0200

    Finished chapter 11, but will need to read through the article about ROPE intervals, as I have never heard of this. This seems like a good time to start splitting up my reading into  1) reading further in the book  2) starting to use what I've learned on for example Kaggle datasets, to make sure it all sticks in the future. I am afraid that if I dont do that, it'll all go to waste quite fast. There's a lot of information to take in.
    At least I should likely take a week after the book, spenting some time just modelling data using my new found knowledge on Kaggle.

--- Chapter-11.R
+++ Chapter-11.R
@@ -11,6 +11,7 @@ load_p <- function(x, character.only = TRUE, ...){
     Recall(x = x, character.only = character.only, ...)
   }
 }
+load_p('tidyposterior')
 load_p('corrr')
 load_p('tidymodels')
 load_p('purrr')
@@ -135,3 +136,105 @@ rsq_estimates %>%
 ## this would cause the result to be biased as they are not independent
 
 # 11.2: Simple hypothesis test
+## A simple method for testing whether there is a difference
+## in the estimated statistics would be to fit a regression on the estimated 
+## measure with the model and fold as dummy variables.
+## For example if we wanted to compare the model with and without splines
+c_lm <- rsq_estimates %>%
+  mutate(difference = `with splines` - `no splines`) %>%
+  lm(formula = difference ~ 1, data = .) %>%
+  tidy(conf.int = TRUE) %>%
+  select(estimate, p.value, starts_with('conf'))
+c_lm
+## From this we can see that there is seemingly a difference between the two estimates (they may be significant)
+
+## Ofc. we could also get the same result with a paired t.test
+## as we know from mixed effect models
+with(rsq_estimates, t.test(`with splines`, `no splines`, paired = TRUE)) %>%
+       tidy() %>%
+       select(estimate, p.value, starts_with('conf'))
+  
+# 11.3: Bayesian Methods 
+## This is (almost) all new to me, I am terribly behind on bayesian statistics. That is a shame.
+## In bayesian methods we have to set up prior information, 
+## prior information is the information that we assume to be somewhat related to the actual data that we are analysing.
+## For example in the model we just fit, a prior information could be the distribution of the various parameters
+## In the book they give an example where
+## e_ij ~ N(0,\sigma)
+## b_j ~ N(0, 10)
+## \sigma ~ exp(1)
+## which we could consider a somewhat "uninformative" prior
+## as the range of b and sigma is quite wide.
+
+## using this prior information we can fit a model, and obtain the posterior distribution
+## of the parameters. These parameters are combinations of the prior and maximum likelihood estimates
+## To gain the extra benefit from fitting stand models, they suggest using a random intercept model (as an example)
+## As they rightly note, this limits the model to note have any variability in intercept, 
+## with is not entirely the case as we see in the visual, but 
+## 1) we only have 10 observations so additional parameters might be hard to evaluate uniquely
+## 2) For the 2 levels of splines and no splines we see only a limited amount of variability in slope.
+
+ames_tm <- bind_cols(ames_folds, 
+                     rsq_estimates %>%
+                       arrange(id) %>%
+                       select(-id))
+ames_tm
+## We can use the tidyposterior package for this purpose.
+## This package is an interface to the rstanarm package, 
+## and this package sets a vector of default priors. 
+## The `perf_mod` function (from tidyposterior) can be used to fit an appropriate bayesian model
+## (note this is all from the book, i have no prior knowledge of rstanarm)
+
+library(rstanarm)
+options(mc.cores = parallel::detectCores() - 1)
+rsq_anova <- perf_mod(
+  ames_tm, 
+  prior_intercept = student_t(df = 1),
+  chains = parallel::detectCores() * 2,
+  iter = 5000, 
+  seed = 2
+)
+## Tidy up the result (seed???)
+model_post <- 
+  tidy(rsq_anova, seed = 35) %>%
+  as_tibble() 
+
+glimpse(model_post)
+
+## Visualize the posterior distribution of Rsquared.
+library(forcats)
+mutate(model_post, model = fct_inorder(model)) %>%
+  ggplot(aes(x = posterior)) +
+  geom_histogram(bins = 50, col = 'white',
+                 fill = 'blue', alpha = 0.4) + 
+  facet_wrap(~ model, ncol = 1) + 
+  labs( x = expression(paste("Posterior for mean ", R^2)))
+  
+## Now that we have the posterior distribution
+## we can compare the means by sampling from the distributions (mcmc) and comparing the difference for the samples
+rsq_diff <- contrast_models(rsq_anova, 
+                            list_1 = "with splines",
+                            list_2 = "no splines", 
+                            seed = 36)
+as_tibble(rsq_diff) %>% 
+  ggplot(aes(x = difference)) + 
+  geom_vline(xintercept = 0, lty = 2) + 
+  geom_histogram(bins = 50, col = "white", fill = "red", alpha = 0.4) + 
+  labs(x = expression(paste("Posterior for mean difference in ", R^2, 
+                            " (splines - no splines)")))
+## We can get a formal test by using summary. Note that "prob" indicates the probability mass, not alpha.
+## Eg. our significance is (1 - prob) / 2
+summary(rsq_diff, prob = 0.90) %>% 
+  select(-starts_with("pract"))
+
+## If we have some practical level of significance that we want to test around 
+## this can be specified in summary using the "size" parameter
+## This is a rope interval.. Definitely need to read the reference on this
+## I have absolutely no idea what that means.
+summary(rsq_diff, size = 0.02) %>%
+  select(starts_with('pract'))
+
+
+# The Bayesian New Statistics: Hypothesis testing, estimation meta-analysis and power analysis from a Bayesian Perspective
+# (article about ROPE intervals)
+
